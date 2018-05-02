b2d600afc5eb1476dfe6c7846d6de980ed41e887
nickmorton <nickmorton@users.noreply.github.com>
2018-05-02 21:50:59 +0100

    Serialize user name filter in query params

--- src/components/user/user-list.component.ts
+++ src/components/user/user-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
 import { Observable } from 'rxjs/Observable';
 import { map } from 'rxjs/operators';
@@ -9,6 +9,7 @@ import { UserService } from './user.service';
 import { IUser, TEntitySort } from '@nickmorton/yes-admin-common';
 
 const PAGE_SIZE = 10;
+const NAME_QUERY_PARAM_KEY = 'name';
 
 @Component({
 	selector: 'app-user-list',
@@ -17,31 +18,48 @@ const PAGE_SIZE = 10;
 export class UserListComponent extends BaseComponent implements OnInit {
 	public users$: Observable<IUser[]>;
 	public tableColumns = ['userName', 'gender', 'dob', 'lastVisited'];
-	private filterNames = new BehaviorSubject<string>('');
+	public nameFilter = '';
+	private nameFilterSubject = new BehaviorSubject<string>('');
 
-	constructor(private router: Router, private userService: UserService) {
+	constructor(private route: ActivatedRoute, private router: Router, private userService: UserService) {
 		super();
 	}
 
 	public ngOnInit() {
-		this.users$ = this.filterNames.pipe(
-			debounceTime(500),
-			distinctUntilChanged(),
-			switchMap(name => {
+		this.nameFilterSubject
+			.pipe(
+				debounceTime(500),
+				distinctUntilChanged()
+			)
+			.subscribe(name => this.router.navigate(['/users'], { queryParams: { name } }));
+
+		this.users$ = this.route.queryParamMap.pipe(
+			switchMap(params => {
+				const name = params.get(NAME_QUERY_PARAM_KEY);
+				if (name !== this.nameFilter) {
+					this.nameFilter = name;
+				}
 				const sort: TEntitySort<IUser> = name ? { surname: 1, forename: 1 } : { lastUpdated: -1 };
 				return this.userService
 					.get({ name, skip: 0, limit: PAGE_SIZE, sort })
 					.pipe(map(r => r.entities));
 			})
 		);
+
+		const queryParamMap = this.route.snapshot.queryParamMap;
+		if (queryParamMap.has(NAME_QUERY_PARAM_KEY)) {
+			const nameFilterValue = queryParamMap.get(NAME_QUERY_PARAM_KEY);
+			this.onNameFilterChange(nameFilterValue);
+			this.nameFilter = nameFilterValue;
+		}
 	}
 
-	public filter(name: string) {
-		this.filterNames.next(name);
+	public onNameFilterChange(name: string) {
+		this.nameFilterSubject.next(name);
 	}
 
 	public view = (user: IUser): boolean => {
-		this.router.navigate(['/users', user._id]);
+		this.router.navigate(['/users', user._id], { queryParams: { ret: this.router.url } });
 		return false;
 	}
 }
