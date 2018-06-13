eba4ea5d555da570814eb9a080f052bcfa8f2ff1
nickmorton <nickmorton@users.noreply.github.com>
2018-06-13 05:39:11 +0100

    Tidy material module imports

--- src/components/shared/shared.module.ts
+++ src/components/shared/shared.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 
 // Material modules.
-import { MatButtonModule } from '@angular/material';
-import { MatCardModule } from '@angular/material';
-import { MatCheckboxModule } from '@angular/material';
-import { MatDatepickerModule, MatNativeDateModule } from '@angular/material';
-import { MatIconModule } from '@angular/material';
-import { MatInputModule } from '@angular/material';
-import { MatListModule } from '@angular/material';
-import { MatRadioModule } from '@angular/material';
-import { MatSelectModule } from '@angular/material';
-import { MatSidenavModule } from '@angular/material';
-import { MatTableModule } from '@angular/material';
-import { MatToolbarModule } from '@angular/material';
+import {
+	MatButtonModule,
+	MatCardModule,
+	MatCheckboxModule,
+	MatDatepickerModule,
+	MatIconModule,
+	MatInputModule,
+	MatListModule,
+	MatNativeDateModule,
+	MatRadioModule,
+	MatSelectModule,
+	MatSidenavModule,
+	MatTableModule,
+	MatToolbarModule
+} from '@angular/material';
 
 @NgModule(
 	{
